25fc3b58c4325e4df00bdb75426b3c982667db70
chakib.bensari <chakib_bensari@hotmail.fr>
2021-02-28 15:40:12 +0100

    Initializing database data with spring

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into category values(NULL, 'Meat');
+insert into category values(NULL, 'Vegetable');
+insert into category values(NULL, 'Fish');
+
+insert into unit_of_measure values(NULL, 'KG');
+insert into unit_of_measure values(NULL, 'CL');
+insert into unit_of_measure values(NULL, 'L');
+insert into unit_of_measure values(NULL, 'COFEE_SPOON');
+insert into unit_of_measure values(NULL, 'SOUPE_SPOON');
