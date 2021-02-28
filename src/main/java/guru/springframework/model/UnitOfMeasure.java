a2c460b6a2d2dccc5b9b730cc3a93846578d64c1
chakib.bensari <chakib_bensari@hotmail.fr>
2021-02-28 12:44:56 +0100

    Add the UnitOfMeasure Entity with the relationship to Ingredient (Unidirectional)

---
+++ src/main/java/guru/springframework/model/UnitOfMeasure.java
@@ -0,0 +1,24 @@
+package guru.springframework.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String unit;
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+}
