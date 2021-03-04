8c95ff8d707d133301acaf083f8952e8a062ece6
chakib.bensari <chakib_bensari@hotmail.fr>
2021-03-04 23:10:15 +0100

    add integration test for CategoryRepository

---
+++ src/test/java/guru/springframework/repositories/CategoryRepositoryTest.java
@@ -0,0 +1,28 @@
+package guru.springframework.repositories;
+
+import guru.springframework.model.Category;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void test(){
+        Optional<Category> cat = categoryRepository.findByDescription("Vegetable");
+
+        assertEquals("Vegetable", cat.get().getDescription());
+    }
+
+}
